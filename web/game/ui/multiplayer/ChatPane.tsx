e6e27854399917ea206d1eaa75352ed4dd464ea3
kiootic <kiootic@gmail.com>
2021-01-02 18:40:33 +0800

    Use icon for buttons

--- web/game/ui/multiplayer/ChatPane.tsx
+++ web/game/ui/multiplayer/ChatPane.tsx
@@ -113,11 +113,32 @@ export const ChatPane = observer<ChatPaneProps>(function RoomPane(props) {
           autoComplete="off"
           ref={register({ required: true })}
         />
-        <button className={styles.action} type="submit">
-          Send
+        <button className={styles.action} type="submit" title="send">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 0 24 24"
+            width="24"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" />
+          </svg>
         </button>
-        <button className={styles.action} type="button" onClick={onClear}>
-          Clear
+        <button
+          className={styles.action}
+          type="button"
+          title="clear"
+          onClick={onClear}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 0 24 24"
+            width="24"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+          </svg>
         </button>
       </form>
     </div>
